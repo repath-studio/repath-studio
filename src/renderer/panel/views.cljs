42ba711312c4fddded29927cdf556487c1e35dfb
Konstantinos Kaloutas <sprocketc@gmail.com>
2025-12-19 18:21:58 +0200

    move styles to view

--- src/renderer/panel/views.cljs
+++ src/renderer/panel/views.cljs
@@ -5,7 +5,21 @@
 
 (defn separator []
   [:> Separator
-   {:class "panel-separator"}])
+   {:class "relative after:block after:absolute after:z-1
+            after:transition-colors after:duration-300 after:delay-300
+            focus:after:delay-0
+            aria-[orientation=horizontal]:h-px
+            aria-[orientation=horizontal]:after:w-full
+            aria-[orientation=horizontal]:after:h-1
+            aria-[orientation=horizontal]:after:top-[-50%]
+            aria-[orientation=vertical]:w-px
+            aria-[orientation=vertical]:after:h-full
+            aria-[orientation=vertical]:after:w-1
+            aria-[orientation=vertical]:after:left-[-50%]
+            data-[separator=active]:after:bg-accent!
+            data-[separator=hover]:after:bg-accent!
+            focus-visible:outline-hidden
+            focus-visible:after:bg-border focus-visible:after:delay-0"}])
 
 (defn panel
   [props & children]
