aa578316f1f5a207ae5386af51ba5419af9ca429
Konstantinos Kaloutas <sprocketc@gmail.com>
2026-04-06 17:12:17 +0300

    remove unneeded action registrations

--- src/renderer/tool/impl/draw/core.cljs
+++ src/renderer/tool/impl/draw/core.cljs
@@ -3,13 +3,10 @@
    [re-frame.core :as rf]
    [renderer.action.events :as-alias action.events]
    [renderer.i18n.views :as i18n.views]
-   [renderer.tool.events :as-alias tool.events]
    [renderer.tool.handlers :as tool.handlers]
    [renderer.tool.hierarchy :as tool.hierarchy]
    [renderer.tool.impl.draw.brush]
-   [renderer.tool.impl.draw.pencil]
-   [renderer.tool.subs :as-alias tool.subs]
-   [renderer.utils.key :as utils.key]))
+   [renderer.tool.impl.draw.pencil]))
 
 (tool.hierarchy/derive-tool ::tool.hierarchy/draw ::tool.hierarchy/tool)
 
@@ -20,21 +17,6 @@
 (defmethod tool.hierarchy/on-activate ::tool.hierarchy/draw
   [db]
   (tool.handlers/set-cursor db "crosshair"))
-
-(rf/dispatch [::action.events/register-action
-              {:id :tool/pencil
-               :label [::label "Pen"]
-               :icon "pencil"
-               :event [::tool.events/activate :pencil]
-               :active [::tool.subs/active? :pencil]}])
-
-(rf/dispatch [::action.events/register-action
-              {:id :tool/brush
-               :label [::label "Brush"]
-               :icon "brush"
-               :event [::tool.events/activate :brush]
-               :active [::tool.subs/active? :brush]
-               :shortcuts [{:keyCode (utils.key/codes "B")}]}])
 
 (rf/dispatch [::action.events/register-action-group
               {:id :tools/draw
