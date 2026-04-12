14253e7675dd38e68906db1fbc307c11ac7ef920
Konstantinos Kaloutas <sprocketc@gmail.com>
2026-04-12 21:12:19 +0300

    prevent default for all registered shortcuts

--- src/renderer/action/effects.cljs
+++ src/renderer/action/effects.cljs
@@ -1,44 +1,7 @@
 (ns renderer.action.effects
   (:require
    [re-frame.core :as rf]
-   [re-pressed.core :as-alias re-pressed]
-   [renderer.utils.key :as utils.key]))
-
-(def prevent-default-keys
-  [{:keyCode (utils.key/codes "EQUALS")}
-   {:keyCode (utils.key/codes "DASH")}
-   {:keyCode (utils.key/codes "RIGHT")}
-   {:keyCode (utils.key/codes "LEFT")}
-   {:keyCode (utils.key/codes "UP")}
-   {:keyCode (utils.key/codes "DOWN")}
-   {:keyCode (utils.key/codes "F1")}
-   {:keyCode (utils.key/codes "F11")}
-   {:keyCode (utils.key/codes "F")
-    :altKey true}
-   {:keyCode (utils.key/codes "E")
-    :altKey true}
-   {:keyCode (utils.key/codes "A")
-    :ctrlKey true}
-   {:keyCode (utils.key/codes "O")
-    :ctrlKey true}
-   {:keyCode (utils.key/codes "S")
-    :ctrlKey true}
-   {:keyCode (utils.key/codes "G")
-    :ctrlKey true}
-   {:keyCode (utils.key/codes "P")
-    :ctrlKey true}
-   {:keyCode (utils.key/codes "W")
-    :ctrlKey true}
-   {:keyCode (utils.key/codes "K")
-    :ctrlKey true}
-   {:keyCode (utils.key/codes "W")
-    :ctrlKey true}
-   {:keyCode (utils.key/codes "D")
-    :ctrlKey true
-    :shiftKey true}
-   {:keyCode (utils.key/codes "T")
-    :ctrlKey true
-    :shiftKey true}])
+   [re-pressed.core :as-alias re-pressed]))
 
 (defn shortcut-modifier-count
   [shortcut]
@@ -62,7 +25,11 @@
                             (into [event] (map vector shortcuts)))))
    :clear-keys []
    :always-listen-keys []
-   :prevent-default-keys prevent-default-keys})
+   ;; Prevent default for all registered shortcuts to avoid conflicts with
+   ;; browser shortcuts. I can't think of a case where we shouldn't do this.
+   ;; In certain cases, the user might have to enter fullscreen mode to use some
+   ;; shortcuts.
+   :prevent-default-keys []})
 
 (rf/reg-fx
  ::update-keydown-rules
