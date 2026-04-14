8fcdcf7a0abc70095a85b2a1f3b24324963e49bb
Konstantinos Kaloutas <sprocketc@gmail.com>
2026-04-14 18:59:46 +0300

    fix indentation

--- src/renderer/a11y/core.cljs
+++ src/renderer/a11y/core.cljs
@@ -33,9 +33,9 @@
                :attrs {:in "SourceGraphic"
                        :type "matrix"
                        :values "0.567, 0.433, 0, 0, 0
-                                   0.558, 0.442, 0, 0, 0
-                                   0, 0.242, 0.758, 0, 0
-                                   0, 0, 0, 1, 0"}}])
+                                0.558, 0.442, 0, 0, 0
+                                0, 0.242, 0.758, 0, 0
+                                0, 0, 0, 1, 0"}}])
 
 (rf/dispatch [::a11y.events/register-filter
               {:id :protanomaly
@@ -44,9 +44,9 @@
                :attrs {:in "SourceGraphic"
                        :type "matrix"
                        :values "0.817, 0.183, 0, 0, 0
-                                   0.333, 0.667, 0, 0, 0
-                                   0, 0.125, 0.875, 0, 0
-                                   0, 0, 0, 1, 0"}}])
+                                0.333, 0.667, 0, 0, 0
+                                0, 0.125, 0.875, 0, 0
+                                0, 0, 0, 1, 0"}}])
 
 (rf/dispatch [::a11y.events/register-filter
               {:id :deuteranopia
@@ -55,9 +55,9 @@
                :attrs {:in "SourceGraphic"
                        :type "matrix"
                        :values "0.625, 0.375, 0, 0, 0
-                                   0.7, 0.3, 0, 0, 0
-                                   0, 0.3, 0.7, 0, 0
-                                   0, 0, 0, 1, 0"}}])
+                                0.7, 0.3, 0, 0, 0
+                                0, 0.3, 0.7, 0, 0
+                                0, 0, 0, 1, 0"}}])
 
 (rf/dispatch [::a11y.events/register-filter
               {:id :deuteranomaly
@@ -66,9 +66,9 @@
                :attrs {:in "SourceGraphic"
                        :type "matrix"
                        :values "0.8, 0.2, 0, 0, 0
-                                   0.258, 0.742, 0, 0, 0
-                                   0, 0.142, 0.858, 0, 0
-                                   0, 0, 0, 1, 0"}}])
+                                0.258, 0.742, 0, 0, 0
+                                0, 0.142, 0.858, 0, 0
+                                0, 0, 0, 1, 0"}}])
 
 (rf/dispatch [::a11y.events/register-filter
               {:id :tritanopia
@@ -77,9 +77,9 @@
                :attrs {:in "SourceGraphic"
                        :type "matrix"
                        :values "0.95, 0.05, 0, 0, 0
-                                   0, 0.433, 0.567, 0, 0
-                                   0, 0.475, 0.525, 0, 0
-                                   0, 0, 0, 1, 0"}}])
+                                0, 0.433, 0.567, 0, 0
+                                0, 0.475, 0.525, 0, 0
+                                0, 0, 0, 1, 0"}}])
 
 (rf/dispatch [::a11y.events/register-filter
               {:id :tritanomaly
@@ -88,9 +88,9 @@
                :attrs {:in "SourceGraphic"
                        :type "matrix"
                        :values "0.967, 0.033, 0, 0, 0
-                                   0, 0.733, 0.267, 0, 0
-                                   0, 0.183, 0.817, 0, 0
-                                   0, 0, 0, 1, 0"}}])
+                                0, 0.733, 0.267, 0, 0
+                                0, 0.183, 0.817, 0, 0
+                                0, 0, 0, 1, 0"}}])
 
 (rf/dispatch [::a11y.events/register-filter
               {:id :achromatopsia
@@ -99,9 +99,9 @@
                :attrs {:in "SourceGraphic"
                        :type "matrix"
                        :values "0.299, 0.587, 0.114, 0, 0
-                                   0.299, 0.587, 0.114, 0, 0
-                                   0.299, 0.587, 0.114, 0, 0
-                                   0, 0, 0, 1, 0"}}])
+                                0.299, 0.587, 0.114, 0, 0
+                                0.299, 0.587, 0.114, 0, 0
+                                0, 0, 0, 1, 0"}}])
 
 (rf/dispatch [::a11y.events/register-filter
               {:id :achromatomaly
@@ -110,6 +110,6 @@
                :attrs {:in "SourceGraphic"
                        :type "matrix"
                        :values "0.618, 0.320, 0.062, 0, 0
-                                   0.163, 0.775, 0.062, 0, 0
-                                   0.163, 0.320, 0.516, 0, 0
-                                   0, 0, 0, 1, 0"}}])
+                                0.163, 0.775, 0.062, 0, 0
+                                0.163, 0.320, 0.516, 0, 0
+                                0, 0, 0, 1, 0"}}])
