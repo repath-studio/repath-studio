e355a104544102e7e222253c6a840ed01ddb760b
Konstantinos Kaloutas <sprocketc@gmail.com>
2026-04-14 18:59:24 +0300

    remove unneeded ns

--- src/renderer/toolbar/db.cljs
+++
@@ -1,9 +0,0 @@
-(ns renderer.toolbar.db
-  (:require
-   [renderer.action.db :refer [ActionId]]
-   [renderer.db :refer [Orientation]]))
-
-(def Toolbar
-  [:map {:closed true}
-   [:orientation Orientation]
-   [:actions [:vector ActionId]]])
