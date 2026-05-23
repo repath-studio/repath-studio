63357edac7944f970275caf98d8f380134dd31e3
Konstantinos Kaloutas <sprocketc@gmail.com>
2026-05-23 14:12:03 +0300

    use persist key on document schema

--- src/renderer/document/db.cljs
+++ src/renderer/document/db.cljs
@@ -19,11 +19,15 @@
 
 (def Document
   [:map {:closed true}
-   [:id {:optional true} DocumentId]
-   [:title {:optional true} DocumentTitle]
-   [:path {:optional true} string?]
+   [:id {:optional true
+         :persist true} DocumentId]
+   [:title {:optional true
+            :persist true} DocumentTitle]
+   [:path {:optional true
+           :persist true} string?]
    [:saved-history-index {:optional true} HistoryIndex]
-   [:version {:optional true} string?]
+   [:version {:optional true
+              :persist true} string?]
    [:hovered-ids {:default #{}} [:set [:or HandleId ElementId]]]
    [:collapsed-ids {:default #{}} [:set ElementId]]
    [:ignored-ids {:default #{}} [:set [:or HandleId ElementId]]]
@@ -31,7 +35,8 @@
    [:rotate {:default 0} number?]
    [:history {:optional true} History]
    [:pan {:default [0 0]} Vec2]
-   [:elements {:default {}} [:map-of ElementId Element]]
+   [:elements {:default {}
+               :persist true} [:map-of ElementId Element]]
    [:centered {:optional true} boolean?]
    [:attrs {:default {:fill "white"
                       :stroke "black"}} [:map-of keyword? string?]]
@@ -41,7 +46,7 @@
 (def PersistedDocument
   (->> Document
        (m/children)
-       (filter (comp #(some #{%} [:id :title :path :version :elements]) first))
+       (filter (comp :persist second))
        (into [:map {:closed true}])))
 
 (def RecentDocument
