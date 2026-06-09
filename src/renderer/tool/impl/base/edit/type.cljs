2c40facfb69e4e06b3d229d6d96b09c67770a27c
Konstantinos Kaloutas <sprocketc@gmail.com>
2026-06-09 10:49:06 +0300

    fix type help text regression

--- src/renderer/tool/impl/base/edit/type.cljs
+++ src/renderer/tool/impl/base/edit/type.cljs
@@ -1,8 +1,9 @@
 (ns renderer.tool.impl.base.edit.type
   (:require
    [renderer.i18n.views :as i18n.views]
-   [renderer.tool.hierarchy :as tool.hierarchy]))
+   [renderer.tool.hierarchy :as tool.hierarchy]
+   [renderer.tool.impl.base.edit.core :as-alias edit]))
 
-(defmethod tool.hierarchy/help [::edit :type]
+(defmethod tool.hierarchy/help [::edit/edit :type]
   []
   (i18n.views/t [::help-type "Enter your text."]))
