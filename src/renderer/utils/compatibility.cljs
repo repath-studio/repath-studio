6dd22087db8cb3d967d8c1a3c0bf726cd4aac5c2
Konstantinos Kaloutas <sprocketc@gmail.com>
2026-07-04 20:49:30 +0300

    fix version regex

--- src/renderer/utils/compatibility.cljs
+++ src/renderer/utils/compatibility.cljs
@@ -4,10 +4,7 @@
 
 (def ver-regex
   "https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string"
-  #"(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)
-    (?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)
-    (?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?
-    (?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$")
+  #"(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$")
 
 (def SemanticVersion
   [:tuple
