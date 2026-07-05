c78a853e05b843aed6f4422c4f23088c5bfb8da8
Konstantinos Kaloutas <sprocketc@gmail.com>
2026-07-05 17:40:31 +0300

    adjust brush size on ctrl down

--- src/renderer/tool/impl/draw/brush.cljs
+++ src/renderer/tool/impl/draw/brush.cljs
@@ -15,6 +15,7 @@
    [renderer.hierarchy :as hierarchy]
    [renderer.history.handlers :as history.handlers]
    [renderer.i18n.views :as i18n.views]
+   [renderer.input.handlers :as input.handlers]
    [renderer.tool.events :as-alias tool.events]
    [renderer.tool.handlers :as tool.handlers]
    [renderer.tool.hierarchy :as tool.hierarchy]
@@ -26,10 +27,33 @@
 
 (defonce brush (reagent/atom nil))
 
+(def min-size 1)
+(def max-size 100)
+
 (rf/reg-fx
  ::set-brush
  (fn [value]
    (reset! brush value)))
+
+(rf/reg-fx
+ ::set-brush-size
+ (fn [value]
+   (reset! brush {:type :element
+                  :tag :circle
+                  :attrs (assoc (:attrs @brush)
+                                :r (/ value 2))})))
+
+(defn update-brush-size
+  [db]
+  (let [brush-size (document.handlers/attr db :brush-size)]
+    (app.handlers/add-fx db [::set-brush-size brush-size])))
+
+(defmethod tool.hierarchy/help [::brush :idle]
+  []
+  [:<>
+   (i18n.views/t [::idle-draw [:div "Click and drag to draw."]])
+   (i18n.views/t [::idle-hold [:div "Hold %1 to adjust brush size."]]
+                 [[views/kbd "Ctrl"]])])
 
 (defmethod tool.hierarchy/tool-options ::brush
   []
@@ -38,8 +62,8 @@
      [:span
       brush-size]
      [views/slider
-      {:min 1
-       :max 100
+      {:min min-size
+       :max max-size
        :step 1
        :title (i18n.views/t [::brush-size "Brush Size"])
        :value [brush-size]
@@ -49,35 +73,55 @@
                                         :brush-size v]))}]]))
 
 (defmethod tool.hierarchy/on-pointer-move [::brush :idle]
-  [db e]
+  [db _e]
   (let [brush-size (document.handlers/attr db :brush-size)
         [x y] (:adjusted-pointer-pos db)
-        pressure (:pressure e)
-        pressure (if (zero? pressure) 1 pressure)
-        r (* (/ brush-size 2) pressure)
         fill (document.handlers/attr db :fill)]
     (app.handlers/add-fx db [::set-brush {:type :element
                                           :tag :circle
                                           :attrs {:cx x
                                                   :cy y
-                                                  :r r
+                                                  :r (/ brush-size 2)
                                                   :fill fill}}])))
+
+(defn adjust-size?
+  [db e]
+  (or (:ctrl-key e)
+      (input.handlers/multi-touch? db)))
 
 (defmethod tool.hierarchy/on-drag-start [::brush :idle]
   [db e]
   (let [brush-size (document.handlers/attr db :brush-size)
         point (string/join " " (conj (:adjusted-pointer-pos db) (:pressure e)))
         fill (document.handlers/attr db :fill)]
-    (-> db
-        (tool.handlers/set-state :create)
-        (element.handlers/add {:type :element
-                               :tag :brush
-                               :attrs {:points point
-                                       :fill fill
-                                       :size brush-size
-                                       :thinning 0.5
-                                       :smoothing 0.5
-                                       :streamline 0.5}}))))
+    (if (adjust-size? db e)
+      (assoc db :last-origin (:pointer-pos e))
+      (-> db
+          (tool.handlers/set-state :create)
+          (element.handlers/add {:type :element
+                                 :tag :brush
+                                 :attrs {:points point
+                                         :fill fill
+                                         :size brush-size
+                                         :thinning 0.5
+                                         :smoothing 0.5
+                                         :streamline 0.5}})))))
+
+(defmethod tool.hierarchy/on-drag [::brush :idle]
+  [db e]
+  (cond-> db
+    (adjust-size? db e)
+    (-> (document.handlers/update-attr
+         :brush-size
+         (fn [size]
+           (let [{:keys [last-origin]} db
+                 [delta-x delta-y] (matrix/sub (:pointer-pos e) last-origin)
+                 delta (if (> (abs delta-x) (abs delta-y)) delta-x (- delta-y))]
+             (if (pos? delta)
+               (min max-size (inc size))
+               (max min-size (dec size))))))
+        (update-brush-size)
+        (assoc :last-origin (:pointer-pos e)))))
 
 (defmethod tool.hierarchy/on-drag [::brush :create]
   [db e]
