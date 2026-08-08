548634ec19912e95e330c53712e85c5f63b177c8
Konstantinos Kaloutas <sprocketc@gmail.com>
2026-08-08 21:42:44 +0300

    enable pretty reporting

--- src/dev.cljs
+++ src/dev.cljs
@@ -4,6 +4,7 @@
    [clojure.pprint :refer (pprint)]
    [clojure.string :as string]
    [malli.dev.cljs :as malli.dev]
+   [malli.dev.pretty :as pretty]
    [re-frame.core :as rf]
    [renderer.app.events :as app.events]))
 
@@ -14,7 +15,7 @@
 
   ;; Enable function instrumentation
   ;; https://github.com/metosin/malli/blob/master/docs/clojurescript-function-instrumentation.md
-  (malli.dev/start!)
+  (malli.dev/start! {:report (pretty/reporter)})
   (malli.dev/stop!)
 
   (pprint (string/trim "This line suppresses some clj-kondo warnings.")))
