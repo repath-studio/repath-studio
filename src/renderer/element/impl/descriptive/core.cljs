e64acba4dc55d62c82f061f1a8a9f95903765408
Konstantinos Kaloutas <sprocketc@gmail.com>
2026-08-22 11:13:47 +0300

    refactor

--- src/renderer/element/impl/descriptive/core.cljs
+++ src/renderer/element/impl/descriptive/core.cljs
@@ -1,7 +1,14 @@
 (ns renderer.element.impl.descriptive.core
   (:require
    [renderer.element.hierarchy :as element.hierarchy]
-   [renderer.element.impl.descriptive.mpath]))
+   [renderer.element.impl.descriptive.mpath]
+   [renderer.hierarchy :as hierarchy]))
+
+(hierarchy/derive! ::element.hierarchy/descriptive
+                   ::element.hierarchy/renderable)
+(hierarchy/derive! :desc ::element.hierarchy/descriptive)
+(hierarchy/derive! :metadata ::element.hierarchy/descriptive)
+(hierarchy/derive! :title ::element.hierarchy/descriptive)
 
 (defmethod element.hierarchy/permitted-content :element.hierarchy/descriptive
   [_el]
