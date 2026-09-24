e7ba0f5746c270d4622f37cd74d107eda531b97e
Konstantinos Kaloutas <sprocketc@gmail.com>
2026-09-24 07:48:59 +0300

    revert default language change

--- src/renderer/shell/db.cljs
+++ src/renderer/shell/db.cljs
@@ -84,7 +84,7 @@
   [:map {:closed true}
    [:verbose {:default false} boolean?]
    [:languages {:default {}} [:map-of ShellLanguageId ShellLanguage]]
-   [:active-language {:default :js} keyword?]
+   [:active-language {:default :cljs} keyword?]
    [:completion {:optional true} [:maybe ShellCompletion]]
    [:paste-allowed {:default false} boolean?]])
 
