192eb1741889e6189a7440ae6d71dba542123cf8
Konstantinos Kaloutas <sprocketc@gmail.com>
2026-09-24 08:09:55 +0300

    fix font-style description and remove incorrect update-attr method

--- src/renderer/attribute/impl/font_style.cljs
+++ src/renderer/attribute/impl/font_style.cljs
@@ -3,24 +3,14 @@
   (:require
    [renderer.attribute.hierarchy :as attribute.hierarchy]
    [renderer.attribute.views :as attribute.views]
-   [renderer.element.hierarchy :as-alias element.hierarchy]
-   [renderer.utils.element :as utils.element]
-   [renderer.utils.length :as utils.length]))
+   [renderer.element.hierarchy :as-alias element.hierarchy]))
 
 (defmethod attribute.hierarchy/description [::element.hierarchy/element
                                             :font-style]
   []
   [::description
-   "The font-size attribute refers to the size of the font from baseline to
-    baseline when multiple lines of text are set solid in a multiline layout
-    environment."])
-
-(defmethod attribute.hierarchy/update-attr [::element.hierarchy/element
-                                            :font-style]
-  [el attribute f & more]
-  (let [font-size (:font-size (utils.element/get-computed-styles el))
-        font-size (utils.length/unit->px font-size)]
-    (assoc-in el [:attrs attribute] (str (apply f font-size more)))))
+   "The font-style attribute specifies whether the text is to be rendered using
+    a normal, italic, or oblique face."])
 
 (defmethod attribute.hierarchy/form-element [::element.hierarchy/element
                                              :font-style]
